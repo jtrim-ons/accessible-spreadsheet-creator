0f76d8f14c3191253ab4230fe0ae7f12440049db
James Trimble <jtrim.ons@gmail.com>
2023-09-25 10:37:33 +0100

    Add more template stuff to index.js

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-export default function unicornFun(input, {postfix = 'rainbows'} = {}) {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
+import fs from 'node:fs';
+import Handlebars from 'handlebars';
+import JSZip from 'jszip';
+import odsTemplate from './template-spreadsheets/template.js';
+
+console.log(odsTemplate);
+
+export default function createZip(odsData) {
+	const zip = new JSZip();
+
+	for (const item of odsTemplate) {
+		const template = Handlebars.compile(item.contents);
+		zip.file(item.filename, template(odsData));
 	}
 
-	return `${input} & ${postfix}`;
+	zip.generateNodeStream({type: 'nodebuffer', streamFiles: true})
+		.pipe(fs.createWriteStream('out.zip'))
+		.on('finish', () => {
+			console.log('out.zip written.');
+		});
 }
+
+const odsData = {
+	tableHeadings: [
+		'Category',
+		'Selected group',
+		'England and Wales'
+	],
+	sheets: [
+		{sheetName: 'Age'},
+		{sheetName: 'Ethnicity'}
+	]
+};
+
+createZip(odsData);
