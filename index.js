4c404e51ecea73b976b57301a270b61eea0559d2
James Trimble <jtrim.ons@gmail.com>
2023-09-25 11:39:34 +0100

    Split index and test files

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
-import fs from 'node:fs';
 import Handlebars from 'handlebars';
-import JSZip from 'jszip';
 import odsTemplate from './template-spreadsheets/template.js';
 
 console.log(odsTemplate);
 
+function cellRef(col, row) {
+	return String.fromCodePoint('A'.codePointAt(0) - 1 + col) + row;
+}
+
 export default function createZip(odsData) {
-	const zip = new JSZip();
+	for (const sheet of odsData.sheets) {
+		sheet.firstTableCell = cellRef(1, 3);
+		sheet.lastTableCell = cellRef(1 + sheet.rowData[0].values.length, 3 + sheet.rowData.length);
+	}
+
+	const result = [];
 
 	for (const item of odsTemplate) {
 		const template = Handlebars.compile(item.contents);
-		zip.file(item.filename, template(odsData));
+		result.push({filename: item.filename, contents: template(odsData)});
 	}
 
-	zip.generateNodeStream({type: 'nodebuffer', streamFiles: true})
-		.pipe(fs.createWriteStream('out.zip'))
-		.on('finish', () => {
-			console.log('out.zip written.');
-		});
+	return result;
 }
-
-const odsData = {
-	tableHeadings: [
-		'Category',
-		'Selected group',
-		'England and Wales'
-	],
-	sheets: [
-		{sheetName: 'Age'},
-		{sheetName: 'Ethnicity'}
-	]
-};
-
-createZip(odsData);
--- test.js
+++ test.js
@@ -1,13 +1,61 @@
-import test from 'ava';
-import unicornFun from './index.js';
+import fs from 'node:fs';
+import JSZip from 'jszip';
+import accessibleSpreadsheetCreator from './index.js';
+//import test from 'ava';
 
-test('main', t => {
-	t.throws(() => {
-		unicornFun(123);
-	}, {
-		instanceOf: TypeError,
-		message: 'Expected a string, got number',
+//test('main', t => {
+//	t.throws(() => {
+//		unicornFun(123);
+//	}, {
+//		instanceOf: TypeError,
+//		message: 'Expected a string, got number',
+//	});
+//
+//	t.is(unicornFun('unicorns'), 'unicorns & rainbows');
+//});
+
+const odsData = {
+	tableHeadings: [
+		'Category',
+		'Selected group',
+		'England and Wales',
+	],
+	sheets: [
+		{
+			sheetName: 'Age',
+			tableName: 'Age',
+			rowData: [
+				{name: 'First category', values: [12.3, 45.6]},
+				{name: 'Second category', values: [12.3, 45.6]},
+			],
+		},
+		{
+			sheetName: 'Ethnicity',
+			tableName: 'Ethnicity',
+			rowData: [
+				{name: 'First category', values: [12.3, 45.6]},
+				{name: 'Second category', values: [12.3, 45.6]},
+				{name: 'Third category', values: [78.9, 45.6]},
+			],
+		},
+	],
+};
+
+const zipFiles = accessibleSpreadsheetCreator(odsData);
+
+const zip = new JSZip();
+for (const {filename, contents} of zipFiles) {
+	zip.file(filename, contents);
+}
+
+zip.generateNodeStream({type: 'nodebuffer', streamFiles: true})
+	.pipe(fs.createWriteStream('out.zip'))
+	.on('finish', () => {
+		console.log('out.zip written.');
+	});
+zip.generateNodeStream({type: 'nodebuffer', streamFiles: true})
+	.pipe(fs.createWriteStream('out.ods'))
+	.on('finish', () => {
+		console.log('out.ods written.');
 	});
 
-	t.is(unicornFun('unicorns'), 'unicorns & rainbows');
-});
