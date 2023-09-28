3a72a6bbbb8a682e640b200039de2f1de6cb10e8
James Trimble <jtrim.ons@gmail.com>
2023-09-28 11:10:42 +0100

    Mark up tables for ToC and characteristics

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function cellRef(col, row) {
 
 export default function createZip(odsData, Handlebars) {
 	odsData.tableCount = odsData.sheets.length + 1;  // add 1 for cover sheet TODO add another for contents?
+	odsData.firstTocCell = cellRef(1, 3);
+	odsData.lastTocCell = cellRef(2, 5);
+	odsData.firstCharacteristicsCell = cellRef(1, 3);
+	odsData.lastCharacteristicsCell = cellRef(2, 5);
 
 	for (const sheet of odsData.sheets) {
 		sheet.firstTableCell = cellRef(1, 3);
