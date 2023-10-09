f5b46fef4e4db9593b4049fec05455e810cc0119
James Trimble <jtrim.ons@gmail.com>
2023-10-09 22:25:36 +0100

    Update META-INF/manifest.xml to match Excel's one

--- scripts/create-patch.sh
+++ scripts/create-patch.sh
@@ -2,6 +2,7 @@
 
 diff -u template-spreadsheets/unzipped-formatted/content.xml template-spreadsheets/handlebars/content.xml > template-spreadsheets/mods.patch.tmp
 diff -u template-spreadsheets/unzipped-formatted/meta.xml template-spreadsheets/handlebars/meta.xml >> template-spreadsheets/mods.patch.tmp
+diff -u template-spreadsheets/unzipped-formatted/META-INF/manifest.xml template-spreadsheets/handlebars/META-INF/manifest.xml >> template-spreadsheets/mods.patch.tmp
 
 sed 's/unzipped-formatted/handlebars/' template-spreadsheets/mods.patch.tmp > template-spreadsheets/mods.patch
 
