4ee8ceba7ece3509e7e50e586cc76f7d6b0b0e4e
James Trimble <jtrim.ons@gmail.com>
2023-10-09 21:41:25 +0100

    Omit a couple of files from the .ods zip, since Excel omits them

--- scripts/create-patch.sh
+++ scripts/create-patch.sh
@@ -2,7 +2,6 @@
 
 diff -u template-spreadsheets/unzipped-formatted/content.xml template-spreadsheets/handlebars/content.xml > template-spreadsheets/mods.patch.tmp
 diff -u template-spreadsheets/unzipped-formatted/meta.xml template-spreadsheets/handlebars/meta.xml >> template-spreadsheets/mods.patch.tmp
-diff -u template-spreadsheets/unzipped-formatted/settings.xml template-spreadsheets/handlebars/settings.xml >> template-spreadsheets/mods.patch.tmp
 
 sed 's/unzipped-formatted/handlebars/' template-spreadsheets/mods.patch.tmp > template-spreadsheets/mods.patch
 
--- scripts/unzip-spreadsheet.sh
+++ scripts/unzip-spreadsheet.sh
@@ -15,4 +15,10 @@ find . -type f -name '*.xml' | while read f; do
     mkdir -p ../unzipped-formatted/$(dirname $f)
     xmllint --format $f > ../unzipped-formatted/$f
 done
+
+# Excel doesn't include these files when it saves as .ods,
+# so hopefully it's okay if we don't either.
+rm ../unzipped-formatted/manifest.rdf
+rm ../unzipped-formatted/settings.xml
+
 cd ../..
