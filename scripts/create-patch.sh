9441dc33689943d45572c37399d5065948f31782
James Trimble <jtrim.ons@gmail.com>
2023-10-11 20:59:12 +0100

    Patch styles.xml

--- scripts/create-patch.sh
+++ scripts/create-patch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 diff -u template-spreadsheets/unzipped-formatted/content.xml template-spreadsheets/mustache/content.xml > template-spreadsheets/mods.patch.tmp
+diff -u template-spreadsheets/unzipped-formatted/styles.xml template-spreadsheets/mustache/styles.xml >> template-spreadsheets/mods.patch.tmp
 diff -u template-spreadsheets/unzipped-formatted/meta.xml template-spreadsheets/mustache/meta.xml >> template-spreadsheets/mods.patch.tmp
 diff -u template-spreadsheets/unzipped-formatted/META-INF/manifest.xml template-spreadsheets/mustache/META-INF/manifest.xml >> template-spreadsheets/mods.patch.tmp
 
